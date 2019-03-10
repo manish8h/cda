bc47f7e3b2b619429dfc95a776195a9fa54bd221
Manish Singh <manish@clearstack.io>
2019-03-10 23:20:41 +0530

    CDA-002 | adding some routes -2

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'names/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'home' => 'pages#home'
+  get 'home' => 'pages#home'
+  resources :names
+
+  
+
+end
